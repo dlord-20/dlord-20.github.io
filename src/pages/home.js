11614295b78076019bacf43afe9865f143729666
dlord-20 <dthomaslord@gmail.com>
2023-02-05 10:44:47 +0300

    Grid is getting set up nicely and adjusts to mobile (one column - might need to change some to 2 columns instead)

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,16 +12,35 @@ export default function Home() {
                 <p>Auto generated text for description</p>
             </div>
             <div className="column-two-even">
-                <p>column 1</p>
+                <p>column 1 - two even</p>
                 <p>column 2</p>
             </div>
-            <div className="column-two-left">
-                <p>column 1</p>
+            <div className="column-two-left-third">
+                <p>column 1 - left third</p>
                 <p>column 2</p>
             </div>
-            <div className="column-two-right">
-                <p>column 1</p>
+            <div className="column-two-left-forth">
+                <p>column 1 - left forth</p>
                 <p>column 2</p>
+            </div>
+            <div className="column-two-right-third">
+                <p>column 1 - right third</p>
+                <p>column 2</p>
+            </div>
+            <div className="column-two-right-forth">
+                <p>column 1 - right forth</p>
+                <p>column 2</p>
+            </div>
+            <div className="column-three-even">
+                <p>column 1 - three even</p>
+                <p>column 2</p>
+                <p>column 3</p>
+            </div>
+            <div className="column-four-even">
+                <p>column 1 - four even</p>
+                <p>column 2</p>
+                <p>column 3</p>
+                <p>column 4</p>
             </div>
         </div>
     );
