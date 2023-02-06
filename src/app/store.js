30b2f0624a118dd1774f829c7483731d5cf6b9d3
dlord-20 <dthomaslord@gmail.com>
2023-02-06 13:24:20 +0300

    Got a functional closing div using the store. Not perfect but does what I need for now

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import checkboxReducer from '../features/checkbox/checkboxSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    checkbox: checkboxReducer
   },
 });
--- src/components/topNav.js
+++ src/components/topNav.js
@@ -1,12 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectStatus } from "../features/checkbox/checkboxSlice";
 import Button from "./button";
 import Menu from "./menu";
+import { changeStatus } from "../features/checkbox/checkboxSlice";
 
 export default function TopNav() {
-
+    const checkboxStatus = useSelector(selectStatus);
+    const dispatch = useDispatch()
     const getButton = () => {
         return (
                 <Button text="Let's Talk" type="a"/>
         )
+    }
+
+    const handleCheckboxClick = () => {
+        console.log('hi');
+        dispatch(changeStatus());
+        if(checkboxStatus) {
+            document.getElementById("checkbox").checked = false;
+        } else {
+            document.getElementById("checkbox").checked = true;
+        }
     }
 
     return (
@@ -22,13 +36,14 @@ export default function TopNav() {
                 {getButton()}
             </div>
             <div className="nav-item nav-mobile">
-                <input type="checkbox" className="toggler"></input>
+                <input type="checkbox" id="checkbox" className="toggler" onClick={() => dispatch(changeStatus())}></input>
                 <div className="hamburger"><div></div></div>
                 <div className="menu">
                     <div className="menu-container">
                         <Menu/>
                         {getButton()}
-                        <div className="breakout"></div>
+                        <div className="breakout" onClick={() => handleCheckboxClick()}>
+                        </div>
                     </div>
                 </div>
             </div>
