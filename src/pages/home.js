a16dba974a9c27f2e86a26b70c958d71d8b3c913
dlord-20 <dthomaslord@gmail.com>
2023-02-12 17:02:04 +0300

    Things are almost now restructured -> Just need to fix what appears first on some column classes especially for bottom-nav

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,54 +7,68 @@ import { experienceData, programmingData } from "../data/data";
 export default function Home() {
 
     return (
-            <div className="container">
-                <Helmet>
-                    <title>Home Title</title>
-                </Helmet>
-                {/* Changing all of this */}
-                <div className="column-one">
-                    <div className="bg-height-20 bg-color-light-blue"></div>
-                    <div>
-                        <h1>Derek Lord</h1>
-                        <p>Auto generated text for description</p>
+        <div>
+            <Helmet>
+                <title>Home Title</title>
+            </Helmet>
+            <div className="bg-color-blue">
+                <div className="container">
+                    <div className="column-one">
+                        <div>
+                            <h1>Derek Lord</h1>
+                            <p>Auto generated text for description</p>
+                        </div>
                     </div>
                 </div>
-                <div className="column-one">
-                    <div className="bg-height-40 bg-color-dark-blue"></div>
-                    <div>
-                        <h1>Radar Chart</h1>
-                        <RenderRadarChart data={programmingData}/>
+            </div>
+            <div className="bg-color-dark-blue">
+                <div className="container">
+                    <div className="column-one">
+                        <div>
+                            <h1>Radar Chart</h1>
+                            <RenderRadarChart data={programmingData}/>
+                        </div>
                     </div>
                 </div>
-                <div className="column-one">
-                    <div className="bg-height-20 bg-color-blue"></div>
-                    <div>
-                        <h1>Bar Chart</h1>
+            </div>
+            <div className="bg-color-blue">
+                <div className="container">
+                    <div className="column-one">
+                        <div className="bg-height-20 bg-color-blue"></div>
+                        <div>
+                            <h1>Bar Chart</h1>
+                        </div>
                     </div>
                 </div>
-                <div className="column-two-even">
-                    <div className="bg-height-40 bg-color-white"></div>
-                    <div>
-                        <h1>Two Even</h1>
-                        <h2>Two Even</h2>
-                        <h3>Two Even</h3>
-                        <h4>Two Even</h4>
-                        <h5>Two Even</h5>
-                        <h6>Two Even</h6>
-                    </div>
-                    <div>
-                        <RenderSimpleBarChart data={experienceData}/>
-                    </div>
-                    <div>
-                        <h4>Skills</h4>
-                        <p>Auto generated short description of my skills</p>
-                    </div>
-                    <div>
+            </div>
+            <div className="bg-color-dark-blue">
+                <div className="container">
+                    <div className="column-two-even">
 
+                        <div>
+                            <h1>Two Even</h1>
+                            <h2>Two Even</h2>
+                            <h3>Two Even</h3>
+                            <h4>Two Even</h4>
+                            <h5>Two Even</h5>
+                            <h6>Two Even</h6>
+                        </div>
+                        <div>
+                            <RenderSimpleBarChart data={experienceData}/>
+                        </div>
+                        <div>
+                            <h4>Skills</h4>
+                            <p>Auto generated short description of my skills</p>
+                        </div>
+                        <div>
+
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className="bg-color-blue">
+                <div className="container">
                     <div className="column-two-left-third">
-                        <div className="bg-height-20 bg-color-gray"></div>
                         <div>
                             <h3>Two Left Third</h3>
                         </div>
@@ -68,90 +82,107 @@ export default function Home() {
                             <Button text="Happy" type="b"/>
                         </div>
                     </div>
-                <div className="column-two-left-forth">
-                    <div className="bg-height-20 bg-color-light-gray"></div>
-                    <div>
-                        <h3>Two Left Forth</h3>
-                    </div>
-                    <div>
-                        <p>column 1</p>
-                    </div>
-                    <div>
-                        <p>column 2</p>
-                    </div>
-                    <div>
-                        <p>Bottom title</p>
-                    </div>
                 </div>
-                <div className="column-two-right-third">
-                    <div className="bg-height-20 bg-color-dark-gray"></div>
-                    <div>
-                        <h3>Two Right Third</h3>
-                    </div>
-                    <div>
-                        <p>column 1</p>
-                    </div>
-                    <div>
-                        <p>column 2</p>
-                    </div>
-                    <div>
-                        <p>Bottom title</p>
-                    </div>
-                </div>
-                <div className="column-two-right-forth">
-                    <div className="bg-height-20 bg-color-black-gray"></div>
-                    <div>
-                        <h3>Two Right Forth</h3>
-                    </div>
-                    <div>
-                        <p>column 1</p>
-                    </div>
-                    <div>
-                        <p>column 2</p>
-                    </div>
-                    <div>
-                        <p>Bottom title</p>
-                    </div>
-                </div>
-                <div className="column-three-even">
-                    <div className="bg-height-20 bg-color-light-gray"></div>
-                    <div>
-                        <h3>Three</h3>
-                    </div>
-                    <div>
-                        <p>column 1</p>
-                    </div>
-                    <div>
-                        <p>column 2</p>
-                    </div>
-                    <div>
-                        <p>column 3</p>
-                    </div>
-                    <div>
-                        <p>Bottom title</p>
-                    </div>
-                </div>
-                <div className="column-four-even">
-                    <div className="bg-height-20 bg-color-dark-gray"></div>
-                    <div>
-                        <h3>Forth Even</h3>
-                    </div>
-                    <div>
-                        <p>column 1</p>
-                    </div>
-                    <div>
-                        <p>column 2</p>
-                    </div>
-                    <div>
-                        <p>column 3</p>
-                    </div>
-                    <div>
-                        <p>column 4</p>
-                    </div>
-                    <div>
-                        <p>Bottom title</p>
+            </div>
+            <div className="bg-color-dark-blue">
+                <div className="container">
+                    <div className="column-two-left-forth">
+                        <div>
+                            <h3>Two Left Forth</h3>
+                        </div>
+                        <div>
+                            <p>column 1</p>
+                        </div>
+                        <div>
+                            <p>column 2</p>
+                        </div>
+                        <div>
+                            <p>Bottom title</p>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="bg-color-blue">
+                <div className="container">
+                    <div className="column-two-right-third">
+                        <div>
+                            <h3>Two Right Third</h3>
+                        </div>
+                        <div>
+                            <p>column 1</p>
+                        </div>
+                        <div>
+                            <p>column 2</p>
+                        </div>
+                        <div>
+                            <p>Bottom title</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-color-dark-blue">
+                <div className="container">
+                    <div className="column-two-right-forth">
+                        <div>
+                            <h3>Two Right Forth</h3>
+                        </div>
+                        <div>
+                            <p>column 1</p>
+                        </div>
+                        <div>
+                            <p>column 2</p>
+                        </div>
+                        <div>
+                            <p>Bottom title</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-color-blue">
+                <div className="container">
+                    <div className="column-three-even">
+                        <div>
+                            <h3>Three</h3>
+                        </div>
+                        <div>
+                            <p>column 1</p>
+                        </div>
+                        <div>
+                            <p>column 2</p>
+                        </div>
+                        <div>
+                            <p>column 3</p>
+                        </div>
+                        <div>
+                            <p>Bottom title</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-color-white">
+                <div className="container">
+                    <div className="column-four-even">
+                        <div>
+                            <h3>Forth Even</h3>
+                        </div>
+                        <div>
+                            <p>column 1</p>
+                        </div>
+                        <div>
+                            <p>column 2</p>
+                        </div>
+                        <div>
+                            <p>column 3</p>
+                        </div>
+                        <div>
+                            <p>column 4</p>
+                        </div>
+                        <div>
+                            <p>Bottom title</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
