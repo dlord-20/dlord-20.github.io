be9b628056e2277bc75708d030e259c852b7de3d
Dlord <dthomaslord@gmail.com>
2023-02-27 21:07:47 +0300

    Added a media query for the timeline and now only shows fade-ins when the timeline is two columns instead of just one

--- src/features/effects/timeline-effects.js
+++ src/features/effects/timeline-effects.js
@@ -3,29 +3,33 @@ import { gsap } from 'gsap';
 
 export const useTimelineEffect = () => {
     useEffect(() => {
+
+        let mm = gsap.matchMedia();
+
         var sections = gsap.utils.toArray('.vertical-timeline-element-content');
         // vertical-timeline-element-icon
-
-        for(let i = 0; i < sections.length; i ++) {
-            let tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: sections[i],
-                    start: "top 80%",
-                    // markers: true,
-                    scrub: 1
+        mm.add("(min-width: 1170px)", () => {
+            for(let i = 0; i < sections.length; i ++) {
+                let tl = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: sections[i],
+                        start: "top 80%",
+                        // markers: true,
+                        scrub: 1
+                    }
+                });
+                if(i % 2 === 0) {
+                    tl.addLabel("start")
+                        .from(sections[i], {x: -50, opacity: 0})
+                        .addLabel("move")
+                        .to(sections[i], {x: 0, opacity: 1});
+                } else {
+                    tl.addLabel("start")
+                        .from(sections[i], {x: 50, opacity: 0})
+                        .addLabel("move")
+                        .to(sections[i], {x: 0, opacity: 1});
                 }
-            });
-            if(i % 2 === 0) {
-                tl.addLabel("start")
-                    .from(sections[i], {x: -50, opacity: 0})
-                    .addLabel("move")
-                    .to(sections[i], {x: 0, opacity: 1});
-            } else {
-                tl.addLabel("start")
-                    .from(sections[i], {x: 50, opacity: 0})
-                    .addLabel("move")
-                    .to(sections[i], {x: 0, opacity: 1});
             }
-        }
+        })
     }
 )};
