fbddd482b3fbb80b2a40542bf27347b40215183a
Dlord <dthomaslord@gmail.com>
2023-03-01 21:40:01 +0300

    Ready to be moved to it's own component and finish customizing there

--- src/pages/home.js
+++ src/pages/home.js
@@ -46,35 +46,29 @@ export default function Home() {
             .from(".buttons", {x: 25, opacity: 0})
             .to(".buttons", {x: 0, opacity: 1}, "-=.55");
 
-            // Starting to mess around with my new intro section
+            // START OF ALPHABET CLOCK
             var columns = gsap.utils.toArray('.alphabet');
             var letters = gsap.utils.toArray('.letter');
 
             let alphabetTl = gsap.timeline();
-            const words = ["MARKETER", "DESIGNER", " ACTOR", "  MODEL", "SPRINTER", "    LDS", "HUSBAND", " FATHER", "8", " 7", "  6", "   5", "    4", "     3", "      2", "       1"]
+            const words = ["MARKETER", "DESIGNER", " ACTOR", "  MODEL", "SPRINTER", "    LDS", "HUSBAND", " FATHER", "   8", "7", "       6", "     5", "  4", "      3", "    2", " 1", "00000000"]
             let num = 0;
-            const clockGap = 1;
+            const clockGap = .75;
 
             for(let k = 0; k < words.length; k++) {
                 for(let i = 0; i < columns.length; i++) {
                     //Get the letter in the current word
                     let letter = words[k].charAt(i);
-
                     for(let j = 0; j < alphabet.length; j++) {
-                        console.log(letter + ' ' + alphabet[j]);
-                        // alphabetTl.from(letters[(i * 26) + j], {opacity: .2}, 0)
                         // Check the current letter with the alphabet
                         if(letter === alphabet[j]) {
-                            // alphabetTl.from(columns[i], { y: - 16 * 12}, 0)
                             // Move the column to the correct letter position to make the word
-                            console.log("Made it")
                             alphabetTl.to(columns[i], { y: (j - 3) * - 16 + 8}, num)
                         } else {
                             // Make all the other letters besides the letters in the current word a little transparent
                             alphabetTl.to(letters[(i * alphabet.length) + j], {opacity: .2}, num)
                             // Reset opacity on all letters except during the last word
                             if(k !== words.length - 1) {
-                                console.log("Running Reset");
                                 alphabetTl.to(letters[(i * alphabet.length) + j], {opacity: 1}, clockGap + num);
                             } 
                         }
@@ -82,6 +76,7 @@ export default function Home() {
                 }
                 num += clockGap;
             }
+            // END OF ALPHABET CLOCK
     }, []);
 
     const getAlphabetColumn = () => {
