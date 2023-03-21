5c84b1b89780e693515cfd9245041e52e66d15c1
Dlord <dthomaslord@gmail.com>
2023-03-21 14:55:52 +0300

    Fixed an error where it wouldn't show up to the number of blogs I wanted if a filter was selected -> Now we need to add a see more button (infinite scroll)

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -55,4 +55,247 @@ export const blogData = [
         // Month is 0-11
         date: new Date(2022, 3, 15)
     },
+    {
+        title: "Using ChatGPT for blogs",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Ai",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(1994, 11, 3)
+    },
+    {
+        title: "another",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Test",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(2022, 3, 15)
+    },
+    {
+        title: "Using ChatGPT for blogs",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Ai",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(1994, 11, 3)
+    },
+    {
+        title: "another",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Test",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(2022, 3, 15)
+    },
+    {
+        title: "Using ChatGPT for blogs",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Ai",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(1994, 11, 3)
+    },
+    {
+        title: "another",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Test",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(2022, 3, 15)
+    },
+    {
+        title: "Using ChatGPT for blogs",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Ai",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(1994, 11, 3)
+    },
+    {
+        title: "another",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Test",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(2022, 3, 15)
+    },
+    {
+        title: "Using ChatGPT for blogs",
+        subtitle: "Efficiently producing better content",
+        previewText: "Achieving your blog goals becomes much easier when you pair your writing with ChatGPT because it creates a great structure...",
+        coverImage: {
+            src: "Derek-Lord-Smiling-Small.jpg",
+            alt: "Derek smiling all happy like"
+        },
+        paragraphs: [
+            <p key="0">this is a paragraph</p>,
+            <p key="1">this is <b>another</b> paragraph</p>,
+            <p key="2">this is the third paragraph</p>
+        ],
+        imagesOrCharts: [
+            {
+                src: "Derek-Lord-Smiling-Small.jpg",
+                alt: "Derek smiling all happy like"
+            }
+        ],
+        categories: [
+            "Ai",
+            "Blogging",
+            "Productivity"
+        ],
+        // Month is 0-11
+        date: new Date(1994, 11, 3)
+    },
 ]
